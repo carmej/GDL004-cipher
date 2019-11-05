191ae470328dd3bbc2eba8ae4dabfb5c57f7b488
carmej <carmen.mejia.pinedo@gmail.com>
2019-11-05 09:44:02 -0600

    funcionalidad de Cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,20 @@
 window.cipher = {
-  // Acá escribe tu código.
+codificar : ( desplazamiento , mensajeACifrar ) => { 
+    let mensajeCifrado =  " " ; 
+    for ( let i =  0 ; i <  mensajeACifrar . length ; i ++ ) { 
+      let resultado = ( mensajeACifrar . charCodeAt (i) -  65  + desplazamiento) %  26  +  65 ;
+      mensajeCifrado + =  Cadena . fromCharCode (resultado); 
+    }
+    return mensajeCifrado;
+
+  },
+
+  decode : ( desplazamiento , mensajeADescifrar ) => { 
+    let mensajeDescifrado =  " " ; 
+    for ( let i =  0 ; i <  mensajeADescifrar . length ; i ++ ) { 
+      let resultado = ( mensajeADescifrar . charCodeAt (i) -  90  - userKey) %  26  +  90 ; 
+      mensajeDescifrado + =  Cadena . fromCharCode (resultado); 
+
+    }
+    volver mensajeDescifrado; 
 };
